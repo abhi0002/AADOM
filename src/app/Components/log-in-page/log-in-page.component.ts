7fe0bfb13b2ca3d3ce314abf52878b6e7510aaa7
abhi0002 <abhipsa.hota002@gmail.com>
2018-06-14 12:26:33 +0530

    can Activate pending

--- src/app/Components/log-in-page/log-in-page.component.ts
+++ src/app/Components/log-in-page/log-in-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpGetService } from '../../Services/http-get.service';
+import { Router } from '@angular/router';
+import { CookieService } from 'angular2-cookie/core';
 
 @Component({
   selector: 'app-log-in-page',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogInPageComponent implements OnInit {
 
-  constructor() { }
+  users: any;
+  userName: string;
+  pass: string;
+  constructor(
+          private getHttp: HttpGetService,
+          private router: Router,
+          private cookiesService: CookieService ) { }
 
   ngOnInit() {
   }
+  onSignin(form: NgForm) {
+  }
 
+  getLogInData(id , password) {
+    this.getHttp.getData('http://172.16.0.119:8080/public/login?email=' + id + '&password=' + password )
+
+    .subscribe((response) => {
+              this.users = response;
+              console.log('logIn');
+              console.log(this.users);
+
+              if (this.users.status === 200) {
+                this.cookiesService.put( 'LoggedCredential', id);
+                this.router.navigate(['dashboard']);
+              console.log('Wrong Credentials');
+              }
+    });
+  }
 }
